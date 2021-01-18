2cb3d1c6c998e48514f6e3fe11a35ac5618bb33c
Lisa1400 <lisamabe631@gmail.com>
2021-01-18 10:47:22 +0200

    created fahrenheit to celsius and celsius to fahrenheit functions

---
+++ taskSeven.js
@@ -0,0 +1,9 @@
+function celsiusTofahrenheit(celsius) {
+  return (fahrTemperature = (celsius * 9) / 5 + 32);
+}
+
+function fahrenheitToCelsius(fahrenheit) {
+  return (celTemperature = ((fahrenheit - 32) * 5) / 9);
+}
+console.log(celsiusTofahrenheit(90))
+console.log(fahrenheitToCelsius(194))
