09aca749e839284818c948ca27ffb09447a6fc51
Lisa1400 <lisamabe631@gmail.com>
2021-01-19 14:45:26 +0200

    changes requested

--- taskSix.js
+++ taskSix.js
@@ -2,7 +2,7 @@ function maximumNumber(numbers) {
   let maxNum = 0;
 
   for (let i = 0; i < numbers.length; i++) {
-    while (numbers[i] > maxNum) {
+    if (numbers[i] > maxNum) {
       maxNum = numbers[i];
     }
   }
