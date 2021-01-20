dc18dfc86f2a6fc80fc9408a7ae1c6c64a6d2235
Lisa1400 <lisamabe631@gmail.com>
2021-01-20 08:27:49 +0200

    added rest parameter

--- taskSix.js
+++ taskSix.js
@@ -1,4 +1,4 @@
-function maximumNumber(numbers) {
+function maximumNumber(...numbers) {
   let maxNum = numbers[0];
 
   for (let i = 0; i < numbers.length; i++) {
@@ -8,4 +8,7 @@ function maximumNumber(numbers) {
   }
   return maxNum;
 }
+console.log(maximumNumber(1, 22, 3, 2))
+
+
 
